26b1859e98674fb2f71439cd4c0e8b95ae11922e
363045841 <161981174@qq.com>
2025-02-09 20:38:41 +0800

    ✨ feat(search): 支持返回索引对应的去重词条

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { SearchService } from './search.service';
-import { error } from 'console';
-
-export interface fuzzySearchClusterErrorRes {
-  error: string;
-}
-export interface fuzzySearchClusterSuccessRes {
-  cluster: number;
-  words: string[];
+import {
+  type fuzzySearchClusterErrorRes,
+  type fuzzySearchClusterSuccessRes,
+} from './search.service';
+/* "id": 84627,
+   "word": "清热",
+   "indexValue": 2,
+   “title”: “当归”,
+   "description" : “匹配文本段落”,
+   “picUrl”: "http:// ...", */
+export interface fuzzySearchFinalRes {
+  id?: number;
+  word: string;
+  indexValue: number;
+  title: string;
+  description: string;
+  picUrl: string;
 }
 
 @Controller('search')
--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -3,9 +3,10 @@ import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IndexTable } from './IndexTable.entity';
+import { medinfo } from '../medinfo/medinfo.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([IndexTable])],
+  imports: [TypeOrmModule.forFeature([IndexTable,medinfo])],
   providers: [SearchService],
   controllers: [SearchController]
 })
