f4ae3f64396a582901ffca61432a0c1ab7864dfe
363045841 <161981174@qq.com>
2025-02-17 22:04:42 +0800

    🎈 perf(main): 切换到fastify

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { FastifyAdapter } from "@nestjs/platform-fastify";
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
   app.setGlobalPrefix("api/v1");
   app.enableCors();
   await app.listen(process.env.PORT ?? 3001);
